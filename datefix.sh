7dabc5b726e707723aae13e4c4f215724f0de019
NafeeJ <nafeejan2@gmail.com>
2021-08-04 02:40:40 -0400

    Better personal solution. Doesn't use ID and lets ordering on same day be done by name which is what the file system will default to. Tests if file epoch is less than the earliest correct date possible

--- datefix.sh
+++ datefix.sh
@@ -4,20 +4,18 @@ modified = 0 # Counts files modified
 for f in OGMods/OGInsta/* # Directory of files to modify. Folder/*
 do
     file="$(basename $f)" # Get full name of file from its path
-    filedate=$(stat -c '%y' $f | cut -d ' ' -f1 | sed -e 's/-//g') # Get YYYYMMDD from current file date
+    filedate="$(date +%s -r $f)" # Get epoch time of file
     echo "TESTING: $f - $filedate" # Print file being tested in format "filename - current date"
     let "tested+=1" # Increment tested counter
-    if [[ "$filedate" == "20191124" ]] # If the current file date is equal to a date (incorrect in this case), then modify
+    if [ "$filedate" -lt "1574640000" ] # If the file's date is less than Nov 25, 2019 00:00:00 (lowest possible date for a file to be correct for me), modify
     then
-        # Extract YYYYMMDD and id from format "(name)-YYYYMMDD-XXXX.(extension)"
+        # Extract YYYYMMDD from format "(name)-YYYYMMDD-XXXX.(extension)"
         date="${file#*-}"
         date="${date%%-*}"
-        id="${file##*-}"
-        id="${id%%.*}"
 
         echo "MODIFYING: $f - $filedate" # Print file being modified in format "filename - current date"
         let "modified+=1" # Increment modified counter
-        touch -a -m -t "${date}${id}.00" "$f" # Modify date accessed and modified with date extracted from name
+        touch -a -m -t "${date}0000.00" "$f" # Modify date accessed and modified with date extracted from name
     fi
 done
 echo "TESTED: $tested" # Print tested
